33de587b940db1729bb62a3bce5a0fee97083fb6
Megadotnet <wintersunair@gmail.com>
2015-06-11 21:56:11 +0800

    Added UserDepartment

---
+++ WebAuth/Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAuth.Areas.Admin.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        // GET: SuperAdmin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
