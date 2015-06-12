b0376d2d9fa5b768a57ed293c2c33bbf392765fd
Megadotnet <wintersunair@gmail.com>
2015-06-12 21:31:08 +0800

    Added PermissionsAdminController

---
+++ WebAuth/Areas/Admin/Controllers/PermissionsAdminController.cs
@@ -0,0 +1,165 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WebAuth.Controllers;
+using WebAuth.Models;
+using Webdiyer.WebControls.Mvc;
+
+namespace WebAuth.Areas.Admin.Controllers
+{
+    public class PermissionsAdminController : BaseController
+    {
+        [Description("权限列表")]
+        public async Task<ActionResult> Index(int index = 1)
+        {
+            var permissions = await _db.Permissions.ToListAsync();
+            //创建ViewModel
+            var permissionViews = new List<PermissionViewModel>();
+            permissions.Each(t =>
+            {
+                var view = Mapper.Map<PermissionViewModel>(t);
+                permissionViews.Add(view);
+            });
+            //排序
+            permissionViews.Sort(new PermissionViewModelComparer());
+            return View(permissionViews.ToPagedList(index, 10));
+        }
+
+        // GET: PermissionsAdmin/Details/5
+        [Description("权限详情")]
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationPermission applicationPermission = _db.Permissions.Find(id);
+            if (applicationPermission == null)
+            {
+                return HttpNotFound();
+            }
+            var view = Mapper.Map<PermissionViewModel>(applicationPermission);
+            return View(view);
+        }
+
+        // GET: PermissionsAdmin/Create
+        [Description("新建权限，列表")]
+       // [GridDataSourceAction]
+        public ActionResult Create()
+        {
+            //创建ViewModel
+            var permissionViews = new List<PermissionViewModel>();
+            //取程序集中权限
+            var allPermissions = _permissionsOfAssembly;
+            //取数据库已有权限
+            var dbPermissions = _db.Permissions.ToList();
+            //取两者差集
+            var permissions = allPermissions.Except(dbPermissions, new ApplicationPermissionEqualityComparer());
+            permissions.Each(t =>
+            {
+                var view = Mapper.Map<PermissionViewModel>(t);
+                permissionViews.Add(view);
+            });
+            //排序
+            permissionViews.Sort(new PermissionViewModelComparer());
+            return View(permissionViews.AsQueryable());
+        }
+
+        [Description("新建权限，保存")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(IEnumerable<PermissionViewModel> data)
+        {
+            if (data == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "参数不能为空");
+            }
+            foreach (var item in data)
+            {
+                //创建权限
+                var permission = Mapper.Map<ApplicationPermission>(item);
+                _db.Permissions.Add(permission);
+            }
+            //保存
+            await _db.SaveChangesAsync();
+            //方法2，使用Newtonsoft.Json序列化结果对象
+            //格式为json字符串，客户端需要解析，即反序列化
+            var result = JsonConvert.SerializeObject(new { Success = true });
+            return new JsonResult { Data = result };
+        }
+
+        // GET: PermissionsAdmin/Edit/5
+        [Description("编辑权限")]
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationPermission applicationPermission = _db.Permissions.Find(id);
+            if (applicationPermission == null)
+            {
+                return HttpNotFound();
+            }
+            var view = Mapper.Map<PermissionViewModel>(applicationPermission);
+            return View(view);
+        }
+
+        // POST: PermissionsAdmin/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [Description("编辑权限，保存")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Controller,Action,Description")] PermissionViewModel view)
+        {
+            if (ModelState.IsValid)
+            {
+                //对象映射
+                var model = Mapper.Map<ApplicationPermission>(view);
+                //修改实体状态
+                _db.Entry(model).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(view);
+        }
+
+        // GET: PermissionsAdmin/Delete/5
+        [Description("删除权限")]
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationPermission applicationPermission = _db.Permissions.Find(id);
+            if (applicationPermission == null)
+            {
+                return HttpNotFound();
+            }
+            var view = Mapper.Map<PermissionViewModel>(applicationPermission);
+            return View(view);
+        }
+
+        // POST: PermissionsAdmin/Delete/5
+        [Description("删除权限，保存")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ApplicationPermission applicationPermission = _db.Permissions.Find(id);
+            _db.Permissions.Remove(applicationPermission);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ WebAuth/Areas/Admin/Controllers/RolePermissionsController.cs
@@ -0,0 +1,181 @@
+﻿using AutoMapper;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WebAuth.Controllers;
+using WebAuth.Models;
+using Webdiyer.WebControls.Mvc;
+
+namespace WebAuth.Areas.Admin.Controllers
+{
+    public class RolePermissionsController : BaseController
+    {
+        // GET: RolePermissions
+        [Description("角色-权限列表")]
+        public ActionResult Index(string roleId, int index = 1)
+        {
+            //取role列表
+            var roles = _roleManager.Roles.ToList();
+            //roleId是否为空
+            if (string.IsNullOrWhiteSpace(roleId))
+            {
+                //取第一个role的id
+                roleId = roles.FirstOrDefault().Id;
+            }
+            //放入viewbag，设置默认值
+            ViewBag.RoleID = new SelectList(roles, "ID", "Name", roleId);
+            //取角色权限列表
+            var permissions = _roleManager.GetRolePermissions(roleId);
+            //创建ViewModel
+            var permissionViews = new List<PermissionViewModel>();
+
+            //var map = Mapper.CreateMap<ApplicationPermission, PermissionViewModel>();
+            permissions.Each(t =>
+            {
+                var view = Mapper.Map<PermissionViewModel>(t);
+                view.RoleId = roleId;
+                permissionViews.Add(view);
+            });
+            //排序
+            permissionViews.Sort(new PermissionViewModelComparer());
+            return View(permissionViews.ToPagedList(index, 10));
+        }
+
+        // GET: RolePermissions/Details/5
+        [Description("角色-权限详情")]
+        public async Task<ActionResult> Details(string roleId, string permissionId)
+        {
+            if (string.IsNullOrWhiteSpace(roleId) || string.IsNullOrWhiteSpace(permissionId))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //取权限
+            ApplicationPermission applicationPermission = _db.Permissions.Find(permissionId);
+            //取角色
+            var role = await _roleManager.FindByIdAsync(roleId);
+            if (applicationPermission == null)
+            {
+                return HttpNotFound();
+            }
+            var view = Mapper.Map<PermissionViewModel>(applicationPermission);
+            view.RoleId = roleId;
+            view.RoleName = role.Name;
+
+            return View(view);
+        }
+
+        // POST: RolePermissions/Create
+        [Description("新建角色-权限,列表")]
+        //[GridDataSourceAction]
+        public ActionResult Create(string roleId)
+        {
+            if (string.IsNullOrWhiteSpace(roleId))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var roles = _roleManager.Roles.ToList();
+            ViewBag.RoleID = new SelectList(roles, "ID", "Name", roleId);
+
+            //取角色权限ID
+            var rolePermissions = _roleManager.GetRolePermissions(roleId);
+            //取全部权限与角色权限的差集
+            var allPermission = _db.Permissions.ToList();
+            var permissions = allPermission.Except(rolePermissions, new ApplicationPermissionEqualityComparer());
+            //创建ViewModel
+            var permissionViews = new List<PermissionViewModel>();
+            permissions.Each(t =>
+            {
+                var view = Mapper.Map<PermissionViewModel>(t);
+
+                permissionViews.Add(view);
+            });
+            //排序
+            permissionViews.Sort(new PermissionViewModelComparer());
+            return View(permissionViews.AsQueryable());
+        }
+
+        // POST: RolePermissions/Edit/5
+        [Description("新建角色-权限，保存")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(string roleId, IEnumerable<ApplicationPermission> data)
+        {
+            if (string.IsNullOrWhiteSpace(roleId) || data.Count() == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //添加Permission
+            foreach (var item in data)
+            {
+                var permission = new ApplicationRolePermission
+                {
+                    RoleId = roleId,
+                    PermissionId = item.Id
+                };
+                //方法1,用set<>().Add()
+                _db.Set<ApplicationRolePermission>().Add(permission);
+            }
+            //保存;
+            var records = await _db.SaveChangesAsync();
+            //方法1，用JsonResult类封装，格式为Json，客户端直接使用
+            var response = new Dictionary<string, bool>();
+            response.Add("Success", true);
+            return new JsonResult { Data = response };
+        }
+
+        // GET: RolePermissions/Delete/5
+        [Description("删除角色-权限")]
+        public async Task<ActionResult> Delete(string roleId, string permissionId)
+        {
+            if (string.IsNullOrWhiteSpace(roleId) || string.IsNullOrWhiteSpace(permissionId))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationPermission applicationPermission = _db.Permissions.Find(permissionId);
+            var role = await _roleManager.FindByIdAsync(roleId);
+            if (applicationPermission == null)
+            {
+                return HttpNotFound();
+            }
+            //创建viewModel
+            var view = Mapper.Map<PermissionViewModel>(applicationPermission);
+            view.RoleId = roleId;
+            view.RoleName = role.Name;
+
+            return View(view);
+        }
+
+        // POST: RolePermissions/Delete/5
+        [Description("删除角色-权限，保存")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string roleId, string permissionId)
+        {
+            if (string.IsNullOrWhiteSpace(roleId) || string.IsNullOrWhiteSpace(permissionId))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (ModelState.IsValid)
+            {
+                //验证role与permission
+                var role = await _roleManager.FindByIdAsync(roleId);
+                var permission = _db.Permissions.Find(permissionId);
+                if (role == null || permission == null)
+                {
+                    return HttpNotFound();
+                }
+                //删除Permission
+                var entity = new ApplicationRolePermission { RoleId = roleId, PermissionId = permissionId };
+                _db.Set<ApplicationRolePermission>().Attach(entity);
+                _db.Entry(entity).State = EntityState.Deleted;
+
+                var result = await _db.SaveChangesAsync();
+            }
+            return RedirectToAction("Index", new { roleId = roleId });
+        }
+    }
+}
