4ae9547489df3423a789ca2557c94cb17b45091c
Megadotnet <wintersunair@gmail.com>
2015-06-26 20:58:19 +0800

    Refactoring UserDepartment class

--- WebAuth/Areas/Admin/Models/UserDepartment.cs
+++ WebAuth/Areas/Admin/Models/UserDepartment.cs
@@ -10,42 +10,6 @@ namespace WebAuth.Models
 {
     using System.Collections.Generic;
 
-    /// <summary>
-    ///     机构
-    /// </summary>
-    public class Department
-    {
-        #region Constructors and Destructors
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="Department" /> class.
-        /// </summary>
-        public Department()
-        {
-            this.Users = new List<UserDepartment>();
-        }
-
-        #endregion
-
-        #region Public Properties
-
-        /// <summary>
-        ///     机构编号
-        /// </summary>
-        public int Id { get; set; }
-
-        /// <summary>
-        ///     机构名称
-        /// </summary>
-        public string Name { get; set; }
-
-        /// <summary>
-        ///     下辖用户列表
-        /// </summary>
-        public virtual ICollection<UserDepartment> Users { get; set; }
-
-        #endregion
-    }
 
     /// <summary>
     ///     The user department.
