eade6080acfeb08aab1e73074ea7c01a77b2f892
Megadotnet <wintersunair@gmail.com>
2015-06-30 20:45:21 +0800

    Added Chat area

---
+++ WebAuth/Areas/Chat/ChatAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebAuth.Areas.Chat
+{
+    public class ChatAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Chat";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Chat_default",
+                "Chat/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
--- WebAuth/Areas/Chat/Controllers/ChatController.cs
+++ WebAuth/Areas/Chat/Controllers/ChatController.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAuth.Controllers;
 
-namespace WebAuth.Controllers
+namespace WebAuth.Areas.Chat.Controllers
 {
     public class ChatController : BaseController
     {
