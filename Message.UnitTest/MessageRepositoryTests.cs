65c55d321db2c3f17dd9bc9cb5a90cc3a22e3fe4
Megadotnet <wintersunair@gmail.com>
2015-07-09 22:02:04 +0800

    Added hub unit testing

--- Message.UnitTest/MessageRepositoryTests.cs
+++ Message.UnitTest/MessageRepositoryTests.cs
@@ -3,6 +3,10 @@ using Message.WebAPI.Services.IRepository;
 using Message.WebAPI.Services.Repository;
 using BusniessEntities.Models;
 using Xunit;
+using WebAuth.Hubs;
+using System.Dynamic;
+using Moq;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace Message.UnitTest
 {
@@ -26,6 +30,27 @@ namespace Message.UnitTest
 
             Assert.True(isSuccess);
 
+        }
+
+        /// <summary>
+        /// Hubses the are mockable via dynamic.
+        /// </summary>
+        /// <see cref="http://www.asp.net/signalr/overview/testing-and-debugging/unit-testing-signalr-applications"/>
+        [Fact]
+        public void HubsAreMockableViaDynamic()
+        {
+            bool sendCalled = false;
+            var hub = new ChatHub();
+            var mockClients = new Mock<IHubCallerConnectionContext<dynamic>>();
+            hub.Clients = mockClients.Object;
+            dynamic all = new ExpandoObject();
+            all.messageReceived = new Action<string, string>((name, message) =>
+            {
+                sendCalled = true;
+            });
+            mockClients.Setup(m => m.All).Returns((ExpandoObject)all);
+            hub.SendMessageToAll("peter", "TestMessage");
+            Assert.True(sendCalled);
         }
     }
 }
