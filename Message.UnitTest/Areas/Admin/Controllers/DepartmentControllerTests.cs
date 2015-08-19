1bb6e2a3f9311cafa39ad69d4e004aa70a147c1d
Megadotnet <wintersunair@gmail.com>
2015-08-19 20:30:35 +0800

    Addded unit test for controller

---
+++ Message.UnitTest/Areas/Admin/Controllers/DepartmentControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using WebAuth.Areas.Admin.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAuth.Areas.Admin.Controllers.Tests
+{
+    public class DepartmentControllerTests
+    {
+        [Fact()]
+        public async Task DetailsTest()
+        {
+            var departmentController = new DepartmentController();
+            var results=await departmentController.Details(0);
+            Assert.NotNull(results);
+        }
+
+        [Fact()]
+        public async void EditTest()
+        {
+            var departmentController = new DepartmentController();
+            var results = await departmentController.Edit(0);
+            Assert.NotNull(results);
+        }
+    }
+}
