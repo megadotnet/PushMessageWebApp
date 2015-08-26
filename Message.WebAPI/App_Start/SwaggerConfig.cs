7669600f98cddacaf8ead791aa0068b4adec5703
Megadotnet <wintersunair@gmail.com>
2015-08-26 21:04:18 +0800

    Fixed bugs on webapi doc

--- Message.WebAPI/App_Start/SwaggerConfig.cs
+++ Message.WebAPI/App_Start/SwaggerConfig.cs
@@ -168,13 +168,13 @@ namespace Message.WebAPI
                         // The file must be included in your project as an "Embedded Resource", and then the resource's
                         // "Logical Name" is passed to the method as shown below.
                         //
-                        c.InjectStylesheet(typeof(InfoBuilder).Assembly, "Swashbuckle.Dummy.SwaggerExtensions.testStyles1.css");
+                        //c.InjectStylesheet(typeof(InfoBuilder).Assembly, "Swashbuckle.Dummy.SwaggerExtensions.testStyles1.css");
 
                         // Use the "InjectJavaScript" option to invoke one or more custom JavaScripts after the swagger-ui
                         // has loaded. The file must be included in your project as an "Embedded Resource", and then the resource's
                         // "Logical Name" is passed to the method as shown above.
                         //
-                        c.InjectJavaScript(thisAssembly, "Swashbuckle.Dummy.SwaggerExtensions.testScript1.js");
+                        //c.InjectJavaScript(thisAssembly, "Swashbuckle.Dummy.SwaggerExtensions.testScript1.js");
 
                         // The swagger-ui renders boolean data types as a dropdown. By default, it provides "true" and "false"
                         // strings as the possible choices. You can use this option to change these to something else,
@@ -185,7 +185,7 @@ namespace Message.WebAPI
                         // By default, swagger-ui will validate specs against swagger.io's online validator and display the result
                         // in a badge at the bottom of the page. Use these options to set a different validator URL or to disable the
                         // feature entirely.
-                        //c.SetValidatorUrl("http://localhost/validator");
+                        //c.SetValidatorUrl("http://localhost:2943/validator");
                         //c.DisableValidator();
 
                         // Use this option to control how the Operation listing is displayed.
