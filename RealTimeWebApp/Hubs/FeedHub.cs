d295a7a296cc2f2b9ae916d853f06ed690594978
Megadotnet <wintersunair@gmail.com>
2015-08-30 11:45:43 +0800

    deleted useless files

--- RealTimeWebApp/Hubs/FeedHub.cs
+++ RealTimeWebApp/Hubs/FeedHub.cs
@@ -5,10 +5,17 @@ using Microsoft.AspNet.SignalR;
 
 namespace RealTimeApp
 {
+    /// <summary>
+    /// FeedHub
+    /// </summary>
     public class FeedHub : Hub
     {
+        /// <summary>
+        /// Initializes this instance.
+        /// </summary>
         public void Init()
         {
+            //intent to empty
         }
     }
 
