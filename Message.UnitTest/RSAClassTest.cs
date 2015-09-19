169b9f274d2495bc2a952a3447c58117a390ed04
Megadotnet <wintersunair@gmail.com>
2015-09-19 12:09:00 +0800

    Added RSAClass

---
+++ Message.UnitTest/RSAClassTest.cs
@@ -0,0 +1,44 @@
+﻿using Message.WebAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Message.UnitTest
+{
+    public class RSAClassTest
+    {
+        [Fact]
+        public void RSAParameters()
+        {
+
+            var rsa = new RSACryptoServiceProvider();
+            var privateParameters = rsa.ExportParameters(true);
+            var publicParameters = rsa.ExportParameters(false);
+
+            //Export private parameter XML representation of privateParameters
+            //object created above
+            Debug.WriteLine(rsa.ToXmlString(true));
+
+            //Export private parameter XML representation of publicParameters
+            //object created above
+            Debug.WriteLine(rsa.ToXmlString(false));
+        }
+
+        [Fact]
+        public void VerifyToken()
+        {
+
+            var token = "User1";
+            var encryptedToken = RSAClass.Encrypt(token);
+            var decryptedToken = RSAClass.Decrypt(encryptedToken);
+
+            Assert.Equal(token, decryptedToken);
+
+        }
+    }
+}
---
+++ Message.WebAPI/Services/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Message.WebAPI.Services
+{
+    public class User
+    {
+
+        public User(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
