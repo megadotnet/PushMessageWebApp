075e704c03dee61e2e56d1cf3d1bd21971c243a1
Megadotnet <wintersunair@gmail.com>
2015-09-20 15:17:34 +0800

    Refactoring

--- Message.WebAPI/Services/RSAClass.cs
+++ Message.WebAPI/Services/RSAClass.cs
@@ -57,40 +57,7 @@ namespace Message.WebAPI.Services
         }
     }
 
-    public class TokenValidationAttribute : ActionFilterAttribute
-    {
-        public override void OnActionExecuting(HttpActionContext actionContext)
-        {
-            string token;
 
-            try
-            {
-                token = actionContext.Request.Headers.GetValues("Authorization-Token").First();
-            }
-            catch (Exception)
-            {
-                actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest)
-                {
-                    Content = new StringContent("Missing Authorization-Token")
-                };
-                return;
-            }
-
-            try
-            {
-                AuthorizedUserRepository.GetUsers().First(x => x.Name == RSAClass.Decrypt(token));
-                base.OnActionExecuting(actionContext);
-            }
-            catch (Exception)
-            {
-                actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden)
-                {
-                    Content = new StringContent("Unauthorized User")
-                };
-                return;
-            }
-        }
-    }
 
     public class AuthorizedUserRepository
     {
---
+++ Message.WebAPI/Services/TokenValidationAttribute.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Message.WebAPI.Services
+{
+    /// <summary>
+    /// TokenValidationAttribute
+    /// </summary>
+    public class TokenValidationAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// Occurs before the action method is invoked.
+        /// </summary>
+        /// <param name="actionContext">The action context.</param>
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            string token;
+
+            try
+            {
+                token = actionContext.Request.Headers.GetValues("Authorization-Token").First();
+            }
+            catch (Exception)
+            {
+                actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent("Missing Authorization-Token")
+                };
+                return;
+            }
+
+            try
+            {
+                AuthorizedUserRepository.GetUsers().First(x => x.Name == RSAClass.Decrypt(token));
+                base.OnActionExecuting(actionContext);
+            }
+            catch (Exception)
+            {
+                actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden)
+                {
+                    Content = new StringContent("Unauthorized User")
+                };
+                return;
+            }
+        }
+    }
+}
