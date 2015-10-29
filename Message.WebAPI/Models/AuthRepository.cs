00cfb650ec55bc4f1fca38723f5c10c55dbd73ca
Megadotnet <wintersunair@gmail.com>
2015-10-29 22:37:29 +0800

    Refactoring

---
+++ Message.WebAPI/Models/AuthRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Message.WebAPI.Models
+{
+
+    public class AuthRepository : IDisposable
+    {
+        private AuthContext _ctx;
+
+        private UserManager<IdentityUser> _userManager;
+
+        public AuthRepository()
+        {
+            _ctx = new AuthContext();
+            _userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(_ctx));
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserModel userModel)
+        {
+            IdentityUser user = new IdentityUser
+            {
+                UserName = userModel.UserName
+            };
+
+            var result = await _userManager.CreateAsync(user, userModel.Password);
+
+            return result;
+        }
+
+        public async Task<IdentityUser> FindUser(string userName, string password)
+        {
+            IdentityUser user = await _userManager.FindAsync(userName, password);
+
+            return user;
+        }
+
+        public void Dispose()
+        {
+            _ctx.Dispose();
+            _userManager.Dispose();
+
+        }
+    }
+
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+
+        }
+    }
+}
