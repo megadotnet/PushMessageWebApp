e5092b035c1dfbe340f9e6672b6d2d89edd302f1
Megadotnet <wintersunair@gmail.com>
2015-11-15 13:46:10 +0800

    Added coments

--- WebAuth/App_Start/HtmlExtensions.cs
+++ WebAuth/App_Start/HtmlExtensions.cs
@@ -1,58 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Mvc.Html;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HtmlExtensions.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   HtmlExtensions
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace WebAuth.App_Start
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+    using System.Web.Mvc;
+    using System.Web.Mvc.Html;
+
     /// <summary>
-    /// HtmlExtensions
+    ///     HtmlExtensions
     /// </summary>
-    /// <see cref="http://www.codeproject.com/Articles/419054/Practical-ASP-NET-MVC-tips"/>
+    /// <see cref="http://www.codeproject.com/Articles/419054/Practical-ASP-NET-MVC-tips" />
     public static class HtmlExtensions
     {
-        public static MvcForm BeginFileForm(this HtmlHelper html)
-        {
-            return html.BeginForm(null, null, FormMethod.Post, new { enctype = "multipart/form-data" });
-        }
+        #region Public Methods and Operators
 
-        public static MvcHtmlString File(this HtmlHelper html, string name, bool multiple)
-        {
-            var tb = new TagBuilder("input");
-            tb.Attributes.Add("type", "file");
-            tb.Attributes.Add("name", name);
-            tb.GenerateId(name);
-
-            if (multiple)
-                tb.Attributes.Add("multiple", "multiple");
-
-            return MvcHtmlString.Create(tb.ToString(TagRenderMode.SelfClosing));
-        }
-
-        public static MvcHtmlString File(this HtmlHelper html, string name)
-        {
-            return html.File(name, false);
-        }
-
-        public static MvcHtmlString MultipleFileFor<TModel, TProperty>(this HtmlHelper<TModel> html,
-               Expression<Func<TModel, TProperty>> expression)
-        {
-            string name = GetFullPropertyName(expression);
-            return html.File(name, true);
-        }
-
-        public static MvcHtmlString FileFor<TModel, TProperty>(this HtmlHelper<TModel> html,
-                      Expression<Func<TModel, TProperty>> expression)
-        {
-            string name = GetFullPropertyName(expression);
-            return html.File(name);
-        }
-
-        public static MvcHtmlString ActionImage(this HtmlHelper html, string imageUrl,
-               string action, string controller, object routeValues, object htmlAttributes)
+        /// <summary>
+        /// The action image.
+        /// </summary>
+        /// <param name="html">
+        /// The html.
+        /// </param>
+        /// <param name="imageUrl">
+        /// The image url.
+        /// </param>
+        /// <param name="action">
+        /// The action.
+        /// </param>
+        /// <param name="controller">
+        /// The controller.
+        /// </param>
+        /// <param name="routeValues">
+        /// The route values.
+        /// </param>
+        /// <param name="htmlAttributes">
+        /// The html attributes.
+        /// </param>
+        /// <returns>
+        /// The <see cref="MvcHtmlString"/>.
+        /// </returns>
+        public static MvcHtmlString ActionImage(
+            this HtmlHelper html, 
+            string imageUrl, 
+            string action, 
+            string controller, 
+            object routeValues, 
+            object htmlAttributes)
         {
             var urlHelper = new UrlHelper(html.ViewContext.RequestContext);
             var link = new TagBuilder("a");
@@ -64,11 +65,52 @@ namespace WebAuth.App_Start
             return MvcHtmlString.Create(link.ToString(TagRenderMode.Normal));
         }
 
+        /// <summary>
+        /// The begin file form.
+        /// </summary>
+        /// <param name="html">
+        /// The html.
+        /// </param>
+        /// <returns>
+        /// The <see cref="MvcForm"/>.
+        /// </returns>
+        public static MvcForm BeginFileForm(this HtmlHelper html)
+        {
+            return html.BeginForm(null, null, FormMethod.Post, new { enctype = "multipart/form-data" });
+        }
+
+        /// <summary>
+        /// The email.
+        /// </summary>
+        /// <param name="html">
+        /// The html.
+        /// </param>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        /// <returns>
+        /// The <see cref="MvcHtmlString"/>.
+        /// </returns>
         public static MvcHtmlString Email(this HtmlHelper html, string name)
         {
             return html.Email(name, string.Empty);
         }
 
+        /// <summary>
+        /// The email.
+        /// </summary>
+        /// <param name="html">
+        /// The html.
+        /// </param>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="MvcHtmlString"/>.
+        /// </returns>
         public static MvcHtmlString Email(this HtmlHelper html, string name, string value)
         {
             var tb = new TagBuilder("input");
@@ -79,24 +121,157 @@ namespace WebAuth.App_Start
             return MvcHtmlString.Create(tb.ToString(TagRenderMode.SelfClosing));
         }
 
-        public static MvcHtmlString EmailFor<TModel, TProperty>(this HtmlHelper<TModel> html,
-               Expression<Func<TModel, TProperty>> expression)
+        /// <summary>
+        /// The email for.
+        /// </summary>
+        /// <param name="html">
+        /// The html.
+        /// </param>
+        /// <param name="expression">
+        /// The expression.
+        /// </param>
+        /// <typeparam name="TModel">
+        /// </typeparam>
+        /// <typeparam name="TProperty">
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="MvcHtmlString"/>.
+        /// </returns>
+        public static MvcHtmlString EmailFor<TModel, TProperty>(
+            this HtmlHelper<TModel> html, 
+            Expression<Func<TModel, TProperty>> expression)
         {
-            var name = GetFullPropertyName(expression);
-            var value = string.Empty;
+            string name = GetFullPropertyName(expression);
+            string value = string.Empty;
 
             if (html.ViewContext.ViewData.Model != null)
+            {
                 value = expression.Compile()((TModel)html.ViewContext.ViewData.Model).ToString();
+            }
 
             return html.Email(name, value);
         }
 
-        static string GetFullPropertyName<T, TProperty>(Expression<Func<T, TProperty>> exp)
+        /// <summary>
+        /// The file.
+        /// </summary>
+        /// <param name="html">
+        /// The html.
+        /// </param>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        /// <param name="multiple">
+        /// The multiple.
+        /// </param>
+        /// <returns>
+        /// The <see cref="MvcHtmlString"/>.
+        /// </returns>
+        public static MvcHtmlString File(this HtmlHelper html, string name, bool multiple)
+        {
+            var tb = new TagBuilder("input");
+            tb.Attributes.Add("type", "file");
+            tb.Attributes.Add("name", name);
+            tb.GenerateId(name);
+
+            if (multiple)
+            {
+                tb.Attributes.Add("multiple", "multiple");
+            }
+
+            return MvcHtmlString.Create(tb.ToString(TagRenderMode.SelfClosing));
+        }
+
+        /// <summary>
+        /// The file.
+        /// </summary>
+        /// <param name="html">
+        /// The html.
+        /// </param>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        /// <returns>
+        /// The <see cref="MvcHtmlString"/>.
+        /// </returns>
+        public static MvcHtmlString File(this HtmlHelper html, string name)
+        {
+            return html.File(name, false);
+        }
+
+        /// <summary>
+        /// The file for.
+        /// </summary>
+        /// <param name="html">
+        /// The html.
+        /// </param>
+        /// <param name="expression">
+        /// The expression.
+        /// </param>
+        /// <typeparam name="TModel">
+        /// </typeparam>
+        /// <typeparam name="TProperty">
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="MvcHtmlString"/>.
+        /// </returns>
+        public static MvcHtmlString FileFor<TModel, TProperty>(
+            this HtmlHelper<TModel> html, 
+            Expression<Func<TModel, TProperty>> expression)
+        {
+            string name = GetFullPropertyName(expression);
+            return html.File(name);
+        }
+
+        /// <summary>
+        /// The multiple file for.
+        /// </summary>
+        /// <param name="html">
+        /// The html.
+        /// </param>
+        /// <param name="expression">
+        /// The expression.
+        /// </param>
+        /// <typeparam name="TModel">
+        /// </typeparam>
+        /// <typeparam name="TProperty">
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="MvcHtmlString"/>.
+        /// </returns>
+        public static MvcHtmlString MultipleFileFor<TModel, TProperty>(
+            this HtmlHelper<TModel> html, 
+            Expression<Func<TModel, TProperty>> expression)
+        {
+            string name = GetFullPropertyName(expression);
+            return html.File(name, true);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// The get full property name.
+        /// </summary>
+        /// <param name="exp">
+        /// The exp.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        /// <typeparam name="TProperty">
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        private static string GetFullPropertyName<T, TProperty>(Expression<Func<T, TProperty>> exp)
         {
             MemberExpression memberExp;
 
             if (!TryFindMemberExpression(exp.Body, out memberExp))
+            {
                 return string.Empty;
+            }
 
             var memberNames = new Stack<string>();
 
@@ -109,37 +284,64 @@ namespace WebAuth.App_Start
             return string.Join(".", memberNames.ToArray());
         }
 
-        static bool TryFindMemberExpression(Expression exp, out MemberExpression memberExp)
+        /// <summary>
+        /// The is conversion.
+        /// </summary>
+        /// <param name="exp">
+        /// The exp.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        private static bool IsConversion(Expression exp)
+        {
+            return exp.NodeType == ExpressionType.Convert || exp.NodeType == ExpressionType.ConvertChecked;
+        }
+
+        /// <summary>
+        /// The try find member expression.
+        /// </summary>
+        /// <param name="exp">
+        /// The exp.
+        /// </param>
+        /// <param name="memberExp">
+        /// The member exp.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        private static bool TryFindMemberExpression(Expression exp, out MemberExpression memberExp)
         {
             memberExp = exp as MemberExpression;
 
             if (memberExp != null)
+            {
                 return true;
+            }
 
             if (IsConversion(exp) && exp is UnaryExpression)
             {
                 memberExp = ((UnaryExpression)exp).Operand as MemberExpression;
 
                 if (memberExp != null)
+                {
                     return true;
+                }
             }
 
             return false;
         }
 
-        static bool IsConversion(Expression exp)
-        {
-            return (exp.NodeType == ExpressionType.Convert || exp.NodeType == ExpressionType.ConvertChecked);
-        }
+        #endregion
 
-        //public static MvcHtmlString Captcha(this HtmlHelper htmlHelper, string textRefreshButton, int length)
-        //{
-        //    return CaptchaHelper.GenerateFullCaptcha(htmlHelper, textRefreshButton, length);
-        //}
+        // public static MvcHtmlString Captcha(this HtmlHelper htmlHelper, string textRefreshButton, int length)
+        // {
+        // return CaptchaHelper.GenerateFullCaptcha(htmlHelper, textRefreshButton, length);
+        // }
 
-        //public static MvcHtmlString Captcha(this HtmlHelper htmlHelper, int length)
-        //{
-        //    return CaptchaHelper.GenerateFullCaptcha(htmlHelper, length);
-        //}
+        // public static MvcHtmlString Captcha(this HtmlHelper htmlHelper, int length)
+        // {
+        // return CaptchaHelper.GenerateFullCaptcha(htmlHelper, length);
+        // }
     }
 }
