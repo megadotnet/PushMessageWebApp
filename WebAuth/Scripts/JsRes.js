1906d19f0642ffccc3301e0aff2b7d69ce44b40b
Megadotnet <wintersunair@gmail.com>
2015-12-01 22:01:03 +0800

    updated js files

--- WebAuth/Scripts/JsRes.js
+++ WebAuth/Scripts/JsRes.js
@@ -6,27 +6,55 @@ var resources = {
   AdminResource: {},
  };
  
+resources.AdminResource.DepartmentViewModelName = { 
+ 'zh-CN': '机构名称',
+ 'en-US': 'Name'
+ };
 resources.AdminResource.AdminLayout_View_AdminText = { 
  'zh-CN': '管理后台',
  'en-US': 'Admin web platform'
  };
-resources.AdminResource.Action = { 
- 'zh-CN': '方法名',
- 'en-US': 'Action'
+resources.AdminResource.PermissionViewModel_RoleName = { 
+ 'zh-CN': '角色名',
+ 'en-US': 'RoleName'
+ };
+resources.AdminResource.ActionRequire = { 
+ 'zh-CN': '请输入方法名',
+ 'en-US': 'Please  add Action name'
  };
 resources.AdminResource.Controller = { 
  'zh-CN': '控制器',
  'en-US': 'Controller'
  };
-resources.AdminResource.PermissionViewModel_Description = { 
- 'zh-CN': '功能描述',
- 'en-US': 'Description'
- };
 resources.AdminResource.ApplicationRole_Description = { 
  'zh-CN': '角色描述',
  'en-US': 'Description'
  };
-resources.AdminResource.ActionRequire = { 
- 'zh-CN': '请输入方法名',
- 'en-US': 'Please  add Action name'
+resources.AdminResource.DepartmentViewModelId = { 
+ 'zh-CN': '部门编号',
+ 'en-US': 'Id'
+ };
+resources.AdminResource.AdminLayout_ActionLink_UserAdmin = { 
+ 'zh-CN': '用户管理',
+ 'en-US': 'UserAdmin'
+ };
+resources.AdminResource.Action = { 
+ 'zh-CN': '方法名',
+ 'en-US': 'Action'
+ };
+resources.AdminResource.AdminLayout_ActionLink_DeptAdmin = { 
+ 'zh-CN': '部门管理',
+ 'en-US': 'Department Admin'
+ };
+resources.AdminResource.PermissionViewModel_Description = { 
+ 'zh-CN': '功能描述',
+ 'en-US': 'Description'
+ };
+resources.AdminResource.RoleViewModel_Name = { 
+ 'zh-CN': '角色名',
+ 'en-US': 'Name'
+ };
+resources.AdminResource.AdminLayout_View_FrontEndText = { 
+ 'zh-CN': '前台',
+ 'en-US': 'FrontEnd'
  };
