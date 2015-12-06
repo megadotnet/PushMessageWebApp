fffb3c6b2b0e0de0128aa05da8a7cceb39e29099
Megadotnet <wintersunair@gmail.com>
2015-12-06 22:20:59 +0800

    Added DisableMvcResponseHeader

--- RealTimeWebApp/Global.asax.cs
+++ RealTimeWebApp/Global.asax.cs
@@ -21,6 +21,8 @@ namespace RealTimeApp
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             MQHubsConfig.RegisterMQListenAndHubs();
 
+            MvcHandler.DisableMvcResponseHeader = true;
+
             //http://www.infragistics.com/community/blogs/devtoolsguy/archive/2015/08/07/12-tips-to-increase-the-performance-of-asp-net-application-drastically-part-1.aspx
             //http://www.infragistics.com/community/blogs/brijmishra/archive/2015/08/21/12-tips-to-increase-the-performance-of-asp-net-application-drastically-part-2.aspx
             // Removing all the view engines
