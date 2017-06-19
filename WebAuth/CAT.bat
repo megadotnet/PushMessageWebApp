19d015c78a7535cf9f5e376edfddb975d3975ba2
megadotnet <wintersunair@gmail.com>
2017-06-19 09:24:42 +0800

    Updated NuGet versions VS2017.sln

---
+++ WebAuth/CAT.bat
@@ -0,0 +1,3 @@
+CATNetCmd64 /file:"J:\MyProject\GitHub\PushMessageWebApp\WebAuth\bin\WebAuth.dll" /search:"J:\MyProject\GitHub\PushMessageWebApp\WebAuth"  /report:"J:\MyProject\GitHub\PushMessageWebApp\WebAuth\bin\report.xml" /config:"C:\Users\Megadotnet\Downloads\CAT.NET\Config\config.xml" 
+
+/rule:"C:\Users\Megadotnet\Downloads\CAT.NET\Rules"
